aeee0f34fc33bc39dd8c7a1a1391f018789fd596
RamezRayyan64 <Ramez.Rayyan64@gmail.com>
2023-03-15 09:46:33 +0430

    commit [rayyan]

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         textTheme: const TextTheme(
           headline4: TextStyle(fontSize: 70),
           headline2: TextStyle(fontSize: 70),
+          headline3: TextStyle(fontSize: 70),
         ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
