d8f74dd12ec08ba3ba7cc97c20101fd09879181e
RamezRayyan64 <Ramez.Rayyan64@gmail.com>
2023-03-15 09:36:19 +0430

    commit3 [main]

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
         textTheme: const TextTheme(
           headline4: TextStyle(fontSize: 70),
         ),
